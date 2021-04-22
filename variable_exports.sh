b27830b2200badd6cd99a243977a8896a182323e
Ubuntu <timp@mlflow.lhfeexnhwuvebf4ud3p32tscka.bx.internal.cloudapp.net>
2021-04-22 13:44:21 +0000

    removed variable_export.sh

--- variable_exports.sh
+++
@@ -1,7 +0,0 @@
-export MLFLOW_S3_ENDPOINT_URL="http://95.217.186.231:9000"
-export MLFLOW_S3_IGNORE_TLS="true"
-export AWS_ACCESS_KEY_ID="mlflowexample"
-export AWS_SECRET_ACCESS_KEY="fancyobjectstorething"
-export MLFLOW_TRACKING_URI="http://training.itu.dk:5000/"
-export MLFLOW_EXPERIMENT_NAME="timp1"
-export Production_ip='http://13.82.0.89:5000/invocations'
